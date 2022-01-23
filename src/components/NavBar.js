f627d8f9b28507fc90973c1fd63d1d52020bdec2
Gabriel Guardia <guardiagabriel-@hotmail.com>
2022-01-23 19:25:15 -0300

    nav bar Galaxias

---
+++ src/components/NavBar.js
@@ -0,0 +1,11 @@
+function navApp() {
+    return <>
+    <NavBar />
+        <ul>
+            <li>Galaxia1</li>
+            <li>Galaxia2</li>
+            <li>GalaxiaTres</li>
+            <li>Galaxia4</li>
+        </ul>
+    </>;
+}
